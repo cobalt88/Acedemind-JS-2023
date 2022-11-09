6bb2a81e22e6eea2ad9d60c21a422705ea6bbc85
cobalt88 <vincentteune@gmail.com>
2022-11-09 15:09:26 -0500

    Start of lesson 282

--- practice-oop-01-starting-setup/practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,46 @@
 class ToolTip {}
 
-class ProjectItem {} 
+class ProjectItem {
+  constructor(id){
+    this.id = id;
+    this.connectSwitchButton();
+    this.connectMoreInfoButton();
+  }
 
-class ProjectList {} 
+  connectMoreInfoButton() {};
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchButton = projectItemElement.querySelector('button: last-of-type');
+    switchButton.addEventListener('click', )
+  };
+} 
 
-class App {}
+class ProjectList {
+  projects = [];
 
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects)
+  }
+  addProject() {
+
+  }
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId)
+    // this.projects.splice(projectIndex, 1)
+    this.projects = this.projects.filter(p => p.is !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsLists = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+  }
+}
+
+App.init()
